4664e439c0b1acc25ca2c5d1aa025a070d4d12af
zedmatrix <zedmatrix007@gmail.com>
2024-08-16 11:42:06 -0700

    Update lambda count.cpp

--- lambda count.cpp
+++ lambda count.cpp
@@ -9,6 +9,7 @@ constexpr void print(const std::string_view str_fmt, auto&&... args) {
 }
 // display the vector
 void disp_v(const auto& v) {
+    print("{}", "Vector: ");
     if (!v.size()) return;
     for(auto e : v) print("{} ", e);
     print("\n");
@@ -19,13 +20,17 @@ int main() {
     auto is_div = [x](int i) { return i % x == 0; };
 
     std::vector<int> v1 = {10, 15, 22, 25, 30, 12, 24};
-    print("Original Vector: ");
+    std::vector<int> v2{};
+    print("Original ");
     disp_v(v1);
     // Correct usage of std::count_if
     auto count = std::count_if(v1.begin(), v1.end(), is_div);
 
     // Correct print statement using your custom print function
     print("div by {} count is {}\n", x, count);
+
+    //copy numbers div by 'x'
+    std::copy_if(v1.begin(), v1.end(), std::back_inserter(v2), is_div);
     disp_v(v2);
     return 0;
 }

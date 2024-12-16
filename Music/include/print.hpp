91ab9d8404512b909f883354aa40684c53b7848f
zedmatrix <zedmatrix007@gmail.com>
2024-12-16 11:24:21 -0800

    Add files via upload

---
+++ Music/include/print.hpp
@@ -0,0 +1,9 @@
+#include <format>
+#include <string>
+
+using std::string;
+
+// format-style print()
+constexpr void print(const std::string_view str_fmt, auto&&... args) {
+    fputs(std::vformat(str_fmt, std::make_format_args(args...)).c_str(), stdout);
+}

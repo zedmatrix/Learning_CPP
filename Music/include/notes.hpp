7afdaf3c728eb76e86e5888f03a890c74162967c
zedmatrix <zedmatrix007@gmail.com>
2024-12-16 17:21:11 -0800

    Update notes.hpp

--- Music/include/notes.hpp
+++ Music/include/notes.hpp
@@ -15,6 +15,22 @@ constexpr float G_Sharp = 415.30;
 constexpr float A = 440.00;
 constexpr float A_Sharp = 466.16;
 constexpr float B = 493.88;
+// Flats
+// constexpr float A_Flat = 415.30;
+// constexpr float B_Flat = 466.16;
+// constexpr float C_Flat = 554.37;
+// constexpr float D_Flat = 523.25;
+// constexpr float E_Flat = 622.25;
+// constexpr float F_Flat = 698.46;
+// constexpr float G_Flat = 740.00;
+// Flats
+constexpr float A_Flat = 783.99;
+constexpr float B_Flat = 932.33;
+constexpr float C_Flat = 932.33;
+constexpr float D_Flat = 1046.50;
+constexpr float E_Flat = 1174.66;
+constexpr float F_Flat = 1318.51;
+constexpr float G_Flat = 1396.91;
 
 // Octave multiplier: Multiply a note's frequency by 2^n to change octaves
 constexpr float OCTAVE_UP = 2.0;

0a080bec6b97ab636052c1f3802bbc0e8222a063
zedmatrix <zedmatrix007@gmail.com>
2024-12-18 18:04:10 -0800

    Update music.cpp

--- Music/src/music.cpp
+++ Music/src/music.cpp
@@ -14,12 +14,12 @@
 int octave = 4;
 int bpm = 120;
 int tempo = 1;
-int duration = 60000 / bpm * tempo;
+int duration = 60000 / (bpm * tempo);
 
 // Map keys to frequencies (from notes.hpp)
 std::map<char, float> keyToFreq = {
     {'c', C}, {'d', D}, {'e', E}, {'f', F}, {'g', G}, {'a', A}, {'b', B},
-    {'C', C_Sharp}, {'D', D_Sharp}, {'E', E_Sharp}, {'F', F_Sharp}, {'G', G_Sharp}, {'A', A_Sharp},
+    {'C', C_Flat}, {'D', D_Flat}, {'E', E_Flat}, {'F', F_Flat}, {'G', G_Flat}, {'A', A_Flat}, {'B', B_Flat},
 };
 
 void generateSineWave(int16_t* buffer, int numSamples, double frequency) {
@@ -29,11 +29,11 @@ void generateSineWave(int16_t* buffer, int numSamples, double frequency) {
 }
 
 void playFrequency(pa_simple* pa, float frequency) {
-    duration = 60000 / bpm * tempo;
+    duration = 60000 / (bpm * tempo);
     int numSamples = SAMPLE_RATE * duration / 1000;
     int16_t* buffer = new int16_t[numSamples];
 
-    std::cout << "Playing: " << frequency << " Hz for " << duration << " ms\n";
+    //std::cout << "Playing: " << frequency << " Hz for " << duration << " ms\n";
     if (!buffer) {
         std::cerr << "Failed to allocate audio buffer\n";
         return;
@@ -46,6 +46,7 @@ void playFrequency(pa_simple* pa, float frequency) {
         delete[] buffer;
         return;
     }
+    std::this_thread::sleep_for(std::chrono::milliseconds(duration));
 }
 
 // Function to interpret and play a song string
@@ -97,7 +98,6 @@ void playSong(pa_simple* pa, const std::string& song) {
         } else {
             std::cerr << "Unknown key: " << key << "\n";
         }
-
     }
 }
 
@@ -117,14 +117,23 @@ int main() {
         return 1;
     }
 
-    // sharps=CDEFGA  normal=cdefgab
-    std::string song = "t2o1cdefgab o2cdefgab o3cdefgab o4cdefgab o5cdefgab o6cdefgab t1o7cdefgab";
-    playSong(pa, song);
+    // flats=CDEFGA  normal=cdefgab
+    // std::string flat_scales = "t1o1CDEFGAB t1o2CDEFGAB t1o3CDEFGAB t1o4CDEFGAB t2o5CDEFGAB t2o6CDEFGAB t2o7CDEFGAB";
+    // playSong(pa, flat_scales);
+    // std::string scales = "t1o1cdefgab t1o2cdefgab t1o3cdefgab t1o4cdefgab t2o5cdefgab t2o6cdefgab t2o7cdefgab";
+    // playSong(pa, scales);
 
-    // for (int i=0; i<3; i++) {
-    //     playSong(pa, song);
-    //     std::this_thread::sleep_for(std::chrono::milliseconds(duration));
-    // }
+    print("Playing Twinkle Twinkle in C\n");
+    std::string twinkle_little_star = "t2o4ccggaagt2o4ffeeddct2o4ggffeedt2o4ggffeedt2o4ccggaagt2o4ffeeddct1 ";
+    playSong(pa, twinkle_little_star);
+
+    print("Playing Jingle Bells in C Major - Moderately Fast Tempo\n");
+    std::string jingle_bells = "t2o4eeeeeeegcdet2o4fffffeeeddedgt2o4eeeeeeegcdet2o4fffffeeggfDct1 ";
+    playSong(pa, jingle_bells);
+
+    print("Playing Happy Brithday in C Major\n");
+    std::string happy_birthday = "t2o4ccddccffEEt2o4ccddccggFFt2o4cccaFEDt2o4bbAFgFt1 ";
+    playSong(pa, happy_birthday);
 
     return 0;
 }

98b0908278fc818e2fb51ba777d6273a9e956a1f
zedmatrix <zedmatrix007@gmail.com>
2025-02-23 20:14:33 -0800

    Add files via upload

--- Humans vs Zombies/main.cpp
+++ Humans vs Zombies/main.cpp
@@ -4,83 +4,16 @@
 #include <ctime>
 #include <format>
 
-//Game Challenge: 1 - Humans vs Zombies
-int main() {
-    std::cout << "Welcome to Humans vs Zombies\n" << "\t\tCombat Simulator\n";
-    std::default_random_engine randomGen(time(0));
-
-    int i = 1;
-    int roll = 10;
-    const float health = 100.0f;
-
-    int numberHumans;
-    float human_damage = 0.0f;
-    float human_health = health;
-    float human_attack = 0.0f;
-
-    int numberZombies;
-    float zombie_damage = 0.0f;
-    float zombie_health = health;
-    float zombie_attack = 0.0f;
-
-    std::cout << "Input Number of Humans: ";
-    std::cin >> numberHumans;
-    if (numberHumans < 1) {
-        numberHumans = 1;
+int getInput(std::string text) {
+    int input;
+    std::cout << std::format("{}: ", text);
+    std::cin >> input;
+    if (input < 10) {
+        input = 10;
     }
-    int startHumans = numberHumans;
-
-    std::cout << "Input Number of Zombies: ";
-    std::cin >> numberZombies;
-    if (numberZombies < 1) {
-        numberZombies = 1;
-    }
-    int startZombies = numberZombies;
-
-    // Humans vs Zombies
-    std::uniform_real_distribution<float> attackRoll(0.0f, 1.0f);
-
-    do {
-        zombie_health = health;
-        human_health = health;
-        std::cout << std::format("Starting Combat: {} Humans With Hit Points: {}\n", numberHumans, human_health);
-        std::cout << std::format("Starting Combat: {} Zombies With Hit Points: {}\n", numberZombies, zombie_health);
-        // TODO: wait between combats
-
-        //Human Health vs Zombie Health
-        while (human_health > 0.0f && zombie_health > 0.0f) {
-            i = 1;
-            while (i < roll) {
-                human_attack = attackRoll(randomGen);
-                zombie_damage = attackRoll(randomGen);
-                if (human_attack < zombie_damage) zombie_damage = human_attack;
-                std::cout << std::format("Human Attacking with {:.4f} producing {:.4f} damage\n", human_attack, zombie_damage);
-                zombie_health -= zombie_damage;
-                i++;
-            }
-
-            i = 1;
-            while (i < roll) {
-                zombie_attack = attackRoll(randomGen);
-                human_damage = attackRoll(randomGen);
-                if (zombie_attack < human_damage) human_damage = zombie_attack;
-                std::cout << std::format("Zombie Attacking with {:.4f} producing {:.4f} damage\n", zombie_attack, human_damage);
-                human_health -= human_damage;
-                i++;
-            }
-            std::cout << std::format("Statistics Human Health: {:.2f} Zombie Health: {:.2f}\n", human_health, zombie_health);
-
-        }
-
-        if (human_health < 0.0f) {
-            numberHumans--;
-            std::cout << std::format("Humans {} Lost\n", numberHumans);
-        } else {
-            numberZombies--;
-            std::cout << std::format("Zombies {} Lost\n", numberZombies);
-        }
-    } while (numberHumans > 0 && numberZombies > 0);
-
+    return input;
+}
+void printSummary(int startHumans, int numberHumans, int startZombies, int numberZombies) {
     if (numberHumans > numberZombies) {
         std::cout << std::format("Humans Win: {}/{} ,Zombies killed: {}, Humans Killed: {}\n",
                                  numberHumans,
@@ -95,6 +28,59 @@ int main() {
                                  startZombies - numberZombies);
     }
 
-    return 0;
+}
+void simulateBattle(const float health, int numberHumans, int numberZombies) {
+    int startHumans = numberHumans;
+    int startZombies = numberZombies;
+    std::default_random_engine randomGen(time(0));
+    std::uniform_real_distribution<float> attackRoll(0.0f, 1.0f);
+    float zombie_health = health;
+    float human_health = health;
+    float attack;
+    std::cout << std::format("Starting Combat: {} Humans With Hit Points: {}\n", numberHumans, human_health);
+    std::cout << std::format("Starting Combat: {} Zombies With Hit Points: {}\n", numberZombies, zombie_health);
+    char turn = 'H';
+
+    while (numberHumans > 0 && numberZombies > 0) {
+        zombie_health = health;
+        human_health = health;
+        std::cout << std::format("\tHumans: {}\t Zombies: {}\n", numberHumans, numberZombies);
+        while (human_health > 0.0f && zombie_health > 0.0f) {
+            attack = attackRoll(randomGen);
+
+            if (turn == 'H') {
+                zombie_health -= attack;
+                //std::cout << std::format("Human Attack: {:.2f}  Zombie Health: {:.2f} \n", attack, zombie_health);
+                turn = 'Z';
+            } else {
+                human_health -= attack;
+                //std::cout << std::format("Zombie Attack: {:.2f}  Human Health: {:.2f} \n", attack, human_health);
+                turn = 'H';
+            }
+        }
+        if (human_health < 0.0f) {
+            numberHumans--;
+        } else {
+            numberZombies--;
+        }
+
+    }
+    printSummary(startHumans, numberHumans, startZombies, numberZombies);
 }
 
+//Game Challenge: 1 - Humans vs Zombies
+int main() {
+    std::cout << "Welcome to Humans vs Zombies\n" << "\t\tCombat Simulator\n";
+
+    const float health = 100.0f;
+    int numberHumans;
+    int numberZombies;
+
+    numberHumans = getInput("Input Number of Humans");
+    numberZombies = getInput("Input Number of Zombies");
+
+    // Humans vs Zombies
+    simulateBattle(health, numberHumans, numberZombies);
+
+    return 0;
+}
